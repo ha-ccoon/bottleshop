5e397e50756eb7454311ee7d841cbe40fac94b35
Hyunsue Yun <yyhs6972@naver.com>
2023-02-20 05:20:47 +0900

    [FEAT] Product DB 입력 구현

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,12 +1,51 @@
 import React from "react";
+import "./Layout.css";
 
 const Layout = ({ children }) => {
   return (
     <div className="App">
       <header>
-        <p className="logo" alt="logo">
-          Bottle Shop
-        </p>
+        <div>
+          <a class="Main_Logo" href="">
+            Bottle Shop
+          </a>
+        </div>
+        <nav class="navbar">
+          <div className="navbar_menu">
+            <ul>
+              <li>
+                <a>Wine</a>
+                <ul>
+                  <li>
+                    <a>RedWine</a>
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    <a>WhiteWine</a>
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    <a>Champane</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Cheeses</a>
+              </li>
+              <li>
+                <a>Q&A</a>
+              </li>
+            </ul>
+            <form>
+              <input className="search_bar" type="search" />
+              <button className="btn btn-outline-secces" type="sumit">
+                Search
+              </button>
+            </form>
+          </div>
+        </nav>
         <h3>header</h3>
       </header>
       <main>{children}</main>
