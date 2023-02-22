e144188863e0130820b83705acdff877e199fa32
Hyunsue Yun <yyhs6972@naver.com>
2023-02-22 20:47:28 +0900

    [CHORE] Product POST, GET 구현

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,20 @@
-'use strict';
-import express from 'express';
+"use strict";
+import express from "express";
 const router = express.Router();
-
-import { postProduct } from '../controller/productController.js';
+import { Product } from "../models/index.js";
+import { postProduct } from "../controller/productController.js";
 
 // http://localhost:8080
-router.get('/', (req, res) => {
-  res.render('index');
+router.get("/", (req, res) => {
+  res.render("index");
 });
 
 // http://localhost:8080/product
-router.get('/product', (req, res) => {
-  res.render('product');
+router.get("/product", async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
 });
 
-router.post('/product', postProduct);
+router.post("/product", postProduct);
 
 export default router;
