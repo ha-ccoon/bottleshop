db647eed592f2e36a33dfffb6be06efd7dd18fc2
minha sohn <codeminhasohn@gmail.com>
2023-02-23 11:48:35 +0900

    [CHORE] order 필터 기능 구현중

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -2,13 +2,18 @@
 import express from 'express';
 const router = express.Router();
 
-import postOrder from '../controller/postOrder.js';
+import { Order } from '../models/index.js';
+import postOrder from '../controller/orders/postOrder.js';
 
-// http://localhost:8080/order
+// http://localhost:8080/order (주문 목록 반환)
 router.get('/', async (req, res) => {
-  res.json(req.body);
+  const orders = await Order.find({});
+  res.json(orders);
 });
 
+// http://localhost:8080/order (주문 추가)
 router.post('/', postOrder);
+
+// http://localhost:8080/order (주문 수정)
 
 export default router;
